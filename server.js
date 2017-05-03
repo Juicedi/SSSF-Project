1e08d2a3f14d4a2e934ab6bfe510f1fa88ecebde
Juicedi <jussi.latvaniemi@gmail.com>
2017-05-03 12:38:18 +0300

    test

--- server.js
+++ server.js
@@ -47,11 +47,7 @@ if (process.env.ENV == 'dev') {
       res.redirect('https://' + req.headers.host + req.url);
     }
   });
-  const server = app.listen(3000);
-  const io = require('socket.io').listen(server);
-  io.sockets.on('connection', (socket) => {
-    socketFunc.initSockets(socket, io);
-  });
+  app.listen(3000);
 }
 
 mongoose.Promise = global.Promise;
